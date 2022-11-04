942fde757249d228da98926ef62360f9bdb93c5e
Roshin James <98834015+roshnjames@users.noreply.github.com>
2022-11-04 10:28:13 +0530

    Add files via upload

---
+++ prgm10.py
@@ -0,0 +1,11 @@
+n=int(input("Enter the limit of your list"))
+l=[]
+for i in range(0,n):
+    ele=int(input("Enter a value"))
+    if(ele>100):
+        l.append("over")
+    else:
+        l.append(ele)
+
+print("Creatd list is")
+print(l)
