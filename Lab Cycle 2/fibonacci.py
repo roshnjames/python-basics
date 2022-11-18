3af24d30b5f203eff85dd887cb53867d0c5603b6
Roshin James <98834015+roshnjames@users.noreply.github.com>
2022-11-18 14:56:24 +0530

    Add files via upload

---
+++ Lab Cycle 2/fibonacci.py
@@ -0,0 +1,17 @@
+def fibonacci(n):
+    num1=1
+    num2=2
+    print(num1,"\n")
+    print(num2,"\n")
+    num3=num1+num2
+    print(num3,"\n")
+    for i in range(0,n-3):
+        num1=num2
+        num2=num3
+        num3=num1+num2
+        print(num3,"\n")
+        
+    
+
+n=int(input("Enter the Fibonacci series limit you want\n"))
+f=fibonacci(n)
